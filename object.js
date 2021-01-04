7affb5668726c37023fb57d6c68eac2f9b89feb8
Md Nazmul Haque <48866588+bpinazmul18@users.noreply.github.com>
2021-01-04 14:09:25 +0600

    first commit

---
+++ object.js
@@ -0,0 +1,52 @@
+let obj = {
+  x: 1,
+  y: 2,
+  z: 3,
+  a: 4,
+}
+
+//**** for loop *******
+// for (var i in obj) {
+//   var value = obj[i]
+//   console.log(i, value)
+// }
+
+
+//************object keys*************
+// var items = Object.keys(obj)
+// // var value = Object.values(obj)
+
+// items.map(keys => {
+//           var value = obj[keys]
+//           console.log(keys, value)
+// })
+
+
+//*********Object.values******
+
+// var items = Object.values(obj); //get values
+
+// items.map(values => {
+//   var key = Object.keys(obj)
+//   console.log(key, values)
+// })
+
+
+///*******Object.getOwnPropertyNames*********///
+
+// var items = Object.getOwnPropertyNames(obj)
+
+// items.map(key => {
+//   var values = Object.values(obj)
+//   console.log(key, values)
+// })
+
+// var items = Object.entries(obj)
+// // console.log(items)
+
+// items.map(key => {
+//   console.log(items)
+// })
+
+
+//hello world//
